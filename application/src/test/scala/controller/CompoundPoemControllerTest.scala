fd47feb72164fed85285c753d3b077b55d4b0fc0
Kate Fulton <katesuzannefulton@gmail.com>
2021-12-09 13:29:24 -0800

    Tests for compound poem controller

--- application/src/test/scala/controller/CompoundPoemControllerTest.scala
+++ application/src/test/scala/controller/CompoundPoemControllerTest.scala
@@ -1,7 +1,54 @@
 package controller
 
+import model.request.CompoundPoemRequest
+import model._
+import org.mockito.Mockito
+import org.mockito.Mockito.{times, verify}
+import org.scalatestplus.mockito.MockitoSugar.mock
+import store.CompoundPoemStore
 import util.BaseSpec
+import ziotestsyntax.ZioTestSyntax.{OngoingZioStubbingHelper, ZioTestHelper}
 
 class CompoundPoemControllerTest extends BaseSpec {
 
+  "save" should "persist the compound poem" in {
+    val title = "title"
+    val author = "author"
+    val line = "line1"
+    val request = CompoundPoemRequest(Some(title), line, line, author, author)
+    val expectedCompoundPoem = CompoundPoem(title,
+      FirstLine(Author(author), Line(line)),
+      SecondLine(Author(author), Line(line)))
+
+    val store = mock[CompoundPoemStore]
+    val controller = new CompoundPoemController(store)
+
+    Mockito.when(store.create(expectedCompoundPoem)).thenSucceed(expectedCompoundPoem)
+
+    val compoundPoem = controller.save(request).unsafeRun
+
+    compoundPoem should be(expectedCompoundPoem)
+    verify(store, times(1)).create(expectedCompoundPoem)
+  }
+
+  it should "handle errors" in {
+    val title = "title"
+    val author = "author"
+    val line = "line1"
+    val request = CompoundPoemRequest(Some(title), line, line, author, author)
+    val expectedCompoundPoem = CompoundPoem(title,
+      FirstLine(Author(author), Line(line)),
+      SecondLine(Author(author), Line(line)))
+    val error = new RuntimeException("Nope")
+
+    val store = mock[CompoundPoemStore]
+    val controller = new CompoundPoemController(store)
+
+    Mockito.when(store.create(expectedCompoundPoem)).thenFail(error)
+
+    val failure = controller.save(request).runFailure
+
+    failure should be(error)
+    verify(store, times(1)).create(expectedCompoundPoem)
+  }
 }
